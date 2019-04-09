b3f208caef52dadf2f6e49a228eefa6f366199a7
彭嘉润 <pengjiarun@vlan-2638-10-19-62-18.student.wireless.sydney.edu.au>
2019-04-09 19:56:39 +1000

    asd

--- JiarunPeng/category_reducer.py
+++ JiarunPeng/category_reducer.py
@@ -5,31 +5,36 @@ import sys
 
 def read_map_output(file):
     for line in file:
-        yield line
+        yield line.strip.split("\t", 1)
 
 
 def category_reducer():
-    v_types = set()
-    data = read_map_output(sys.stdin)
-    for line in data:
-        v_types.add(line[0].strip())
-
-    for v_type in v_types:
-        country_count = set()
-        video_count = set()
-        for category, vnc in data:
-            video_id = vnc[0:-2]
-            if v_type == category:
-                country_count.add(vnc)
-                video_count.add(video_id)
-        division_1 = len(country_count)
-        division_2 = len(video_count)
-        if division_2 == 0:
-            result = 0.0
-        else:
-            result = round((float(division_1) / float(division_2)), 2)
-        print("{}\t:{}".format(v_type, result))
-
-
+    country_count = set()
+    video_count = set()
+    current_category = ""
+    for category, vnc in read_map_output(sys.stdin):
+        video_id = vnc[0:-2]
+        if current_category != category:
+            if current_category !="":
+                division_1 = len(country_count)
+                division_2 = len(video_count)
+                if division_2 == 0:
+                    result = 0.0
+                else:
+                    result = round((float(division_1) / float(division_2)), 2)
+                print("{}:{}".format(category, result))
+            current_category = category
+            country_count = set()
+            video_count = set()
+        country_count.add(vnc)
+        video_count.add(video_id)
+    if current_category != "":
+    division_1 = len(country_count)
+    division_2 = len(video_count)
+    if division_2 == 0:
+        result = 0.0
+    else:
+        result = round((float(division_1) / float(division_2)), 2)
+    print("{}:{}".format(current_category, result))
 if __name__ == "__main__":
     category_reducer()
--- JiarunPeng/practice_driver.sh
+++ JiarunPeng/practice_driver.sh
@@ -9,9 +9,9 @@ fi
 hadoop jar /usr/lib/hadoop/hadoop-streaming-2.8.5-amzn-1.jar \
 -D mapreduce.job.reduces=3 \
 -D mapreduce.job.name='COMP5349_assignment1' \
--file category_mapper.py \
--mapper category_mapper.py \
--file category_reducer.py \
--reducer category_reducer.py \
+-file practice_mapper.py \
+-mapper practice_mapper.py \
+-file practice_reducer.py \
+-reducer practice_reducer.py \
 -input $1 \
 -output $2
