188ea8bab370ea302d22b0ab76b75b9165ef8556
彭嘉润 <pengjiarun@vlan-2638-10-19-62-18.student.wireless.sydney.edu.au>
2019-04-09 15:36:42 +1000

    asdasd

--- JiarunPeng/category_driver.sh
+++ JiarunPeng/category_driver.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-hadoop jar /usr/lib/hadoop/hadoop-streaming-2.8.5-amzn-2.jar \
+if [ $# -ne 2 ]; then
+echo "Invalid number of parameters!"
+echo "Usage: ./tag_driver_combiner.sh [input_location] [output_location]"
+exit 1
+fi
+
+hadoop jar /usr/lib/hadoop/hadoop-streaming-2.8.5-amzn-1.jar \
+-D mapreduce.job.reduces=3 \
 -D mapreduce.job.name='COMP5349_assignment1' \
 -file practice_mapper.py \
--mapper "practice_mapper.py" \
+-mapper practice_mapper.py \
 -file practice_reducer.py \
--reducer "practice_reducer.py" \
+-reducer practice_reducer.py \
 -input $1 \
 -output $2
 
